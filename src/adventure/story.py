9321a63f9883f2b97410d861acca4e0f1a0b41e8
Steven Loy <231585531+sloy1cosc381@users.noreply.github.com>
2025-10-30 01:50:38 +0000

    Added colors to text using rich

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,5 +1,9 @@
 from adventure.utils import read_events_from_file
 import random
+from rich import print
+from rich.console import Console
+
+console=Console()
 
 def step(choice: str, events):
     random_event = random.choice(events)
@@ -9,20 +13,20 @@ def step(choice: str, events):
     elif choice == "right":
         return right_path(random_event)
     else:
-        return "You stand still, unsure what to do. The forest swallows you."
+        return "[green4]You stand still, unsure what to do.[/green4] [dark_red]The forest swallows you.[/dark_red]"
 
 def left_path(event):
-    return "You walk left. " + event
+    return "[green4]You walk[/green4] [blue3]left[/blue3][green4]. " + event + "[/green4]"
 
 def right_path(event):
-    return "You walk right. " + event
+    return "[green4]You walk[/green4] [magenta]right[/magenta][green4]. " + event + "[/green4]"
 
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
 
-    print("You wake up in a dark forest. You can go left or right.")
+    print("[green4]You wake up in a dark forest. You can go[/green4] [blue3]left[/blue3] [green4]or[/green4] [magenta]right[/magenta][green4].[/green4]")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
+        choice = console.input("Which direction do you choose? ([blue3]left[/blue3]/[magenta]right[/magenta]/[dark_red]exit[/dark_red]): ")
         choice = choice.strip().lower()
         if choice == 'exit':
             break
